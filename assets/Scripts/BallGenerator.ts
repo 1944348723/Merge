9df5eb446fe55ad25ffa65fc8289796a267167ff
1944348723 <133476454+1944348723@users.noreply.github.com>
2025-05-30 17:27:33 +0800

    Implementing merging

--- assets/Scripts/BallGenerator.ts
+++ assets/Scripts/BallGenerator.ts
@@ -1,4 +1,6 @@
-import { _decorator, Component, Node, EventTouch, Input, input, instantiate, view, Prefab, RigidBody2D, UITransform, Sprite, Vec2, RigidBody} from 'cc';
+import { _decorator, Component, Node, EventTouch, Input, input, instantiate, view, Prefab, RigidBody2D, Vec2 } from 'cc';
+import { BallType } from './BallType';
+import { BallManager } from './BallManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('BallGenerator')
@@ -23,41 +25,53 @@ export class BallGenerator extends Component {
     protected start(): void {
         this.generatePositionX = this.node.worldPositionX
         this.generatePositionY = this.node.worldPositionY + view.getVisibleSize().height * 3 / 8;
-        this.ballToDrop = this.generateBall(this.generatePositionX, this.generatePositionY);
+        this.ballToDrop = this.generateRandomBall(this.generatePositionX, this.generatePositionY);
     }
 
     onTouchMove(event: EventTouch) {
         if (!this.ballToDrop) {
             return;
         }
+        //TODO: 快速移动时会穿出屏幕
         this.ballToDrop.worldPositionX += event.getDeltaX();
     }
 
     onTouchEnd() {
         // 让当前预览的球掉落
-        const rigidBody: RigidBody2D = this.ballToDrop?.getComponent(RigidBody2D) as RigidBody2D;
-        if (rigidBody) {
-            rigidBody.gravityScale = 1;
-            rigidBody.applyLinearImpulse(new Vec2(0, -0.1), rigidBody.getWorldCenter(new Vec2()), true);
-        }
+        this.dropBall(this.ballToDrop);
         this.ballToDrop = null;
-
-        // 稍微等一会再生成下一个
         this.scheduleOnce(() => {
-            this.ballToDrop = this.generateBall(this.generatePositionX, this.generatePositionY);
-            console.log(`Ball instantiated:`, this.ballToDrop);
+            this.ballToDrop = this.generateRandomBall(this.generatePositionX, this.generatePositionY);
         }, 1)
     }
 
-    generateBall(x:number, y:number): Node {
-        const randomIndex = Math.floor(Math.random() * this.ballPrefabs.length);
-        const ball = instantiate(this.ballPrefabs[randomIndex]);
-        const rigidBody:RigidBody2D = ball.getComponent(RigidBody2D);
-        rigidBody.gravityScale = 0;
+    private generateRandomBall(x:number, y:number) {
+        const type:BallType = Math.floor(Math.random() * this.ballPrefabs.length);
+        return this.generateBall(x, y, type);
+    }
+
+    private generateBall(x:number, y:number, type: BallType): Node {
+        const ball = instantiate(this.ballPrefabs[type]);
+        ball.getComponent(BallManager).init(type);
         ball.setParent(this.node.parent);
+
         ball.worldPositionX = x;
         ball.worldPositionY = y;
+
+        const rigidBody:RigidBody2D = ball.getComponent(RigidBody2D);
+        rigidBody.gravityScale = 0;
+
         return ball;
+    }
+
+    private dropBall(ball: Node) {
+        const rigidBody: RigidBody2D = ball?.getComponent(RigidBody2D);
+        if (rigidBody) {
+            rigidBody.gravityScale = 1;
+            const downwardImpulse = new Vec2(0, -0.1);
+            let rigidBodyCenter = rigidBody.getWorldCenter(new Vec2());
+            rigidBody.applyLinearImpulse(downwardImpulse, rigidBodyCenter, true);
+        }
     }
 }
 
