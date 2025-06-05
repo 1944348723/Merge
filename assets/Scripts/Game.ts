51fbc686f0eecfda36b701e12080a1351605e546
1944348723 <133476454+1944348723@users.noreply.github.com>
2025-06-05 11:28:28 +0800

    Refactor: Redesigned the architecture

---
+++ assets/Scripts/Game.ts
@@ -0,0 +1,56 @@
+import { _decorator, Component, director, Node, UITransform, view } from 'cc';
+import { EventType } from './EventType';
+import { BallGenerator } from './BallGenerator';
+import { Controller } from './Controller';
+import { BallType } from './BallType';
+import { BallManager } from './BallManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('Game')
+export class Game extends Component {
+    @property(BallGenerator)
+    ballGenerator: BallGenerator | null = null;
+
+    @property(Controller)
+    controller: Controller | null = null;
+
+    private generatePositionX: number = 0; 
+    private generatePositionY: number = 0;
+
+    protected start(): void {
+        const uitransform = this.getComponent(UITransform);
+        if (uitransform) {
+            this.controller?.setBounds(0, uitransform.width);
+            console.log(this.controller);
+        }
+
+        this.generatePositionX = this.node.worldPositionX
+        this.generatePositionY = this.node.worldPositionY + view.getVisibleSize().height * 3 / 8;
+        const ball = this.ballGenerator.generateRandomBall(this.generatePositionX, this.generatePositionY);
+        this.controller.currentBall = ball;
+    }
+
+    protected onLoad(): void {
+        director.on(EventType.PLAYER_DROPPED_BALL, this.onPlayerDroppedBall, this);
+        director.on(EventType.BALL_MERGED, this.onBallMerged, this);
+    }
+
+    protected onDestroy(): void {
+        director.off(EventType.PLAYER_DROPPED_BALL, this.onPlayerDroppedBall, this);
+        director.off(EventType.BALL_MERGED, this.onBallMerged, this);
+    }
+
+    onPlayerDroppedBall() {
+        this.scheduleOnce(() => {
+            const ball = this.ballGenerator.generateRandomBall(this.generatePositionX, this.generatePositionY);
+            this.controller.currentBall = ball;
+        }, 1);
+    }
+
+    onBallMerged(x: number, y: number, type: BallType) {
+        const newBall = this.ballGenerator.generateBall(x, y, type + 1);
+        newBall.getComponent(BallManager)?.drop();
+    }
+}
+
+
