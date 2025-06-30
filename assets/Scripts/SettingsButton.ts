6c76a225af388ef1fb79257505ca0d9a3ee7cbb5
1944348723 <133476454+1944348723@users.noreply.github.com>
2025-06-30 11:00:13 +0800

    refactor: refactor settings system

---
+++ assets/Scripts/SettingsButton.ts
@@ -0,0 +1,20 @@
+import { _decorator, Component, Input, Node } from 'cc';
+import { EventType } from './EventType';
+const { ccclass, property } = _decorator;
+
+@ccclass('SettingsButton')
+export class SettingsButton extends Component {
+    protected onLoad(): void {
+        this.node.on(Input.EventType.TOUCH_END, this.onSettingsButtonClicked, this)
+    }
+
+    protected onDestroy(): void {
+        this.node.off(Input.EventType.TOUCH_END, this.onSettingsButtonClicked, this);
+    }
+
+    onSettingsButtonClicked() {
+        this.node.parent.emit(EventType.SETTINGS_BUTTON_CLICKED);
+    }
+}
+
+
