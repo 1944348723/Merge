7a763c992b3373015cfc84bfd74632ae18fb29fa
1944348723 <133476454+1944348723@users.noreply.github.com>
2025-06-30 11:01:53 +0800

    refactor: refactor settings system

--- assets/Scripts/SettingsPanel.ts
+++ assets/Scripts/SettingsPanel.ts
@@ -8,11 +8,18 @@ export class SettingsPanel extends Component {
     closeButton: Node = null;
 
     protected onLoad(): void {
+        this.node.parent.on(EventType.SETTINGS_BUTTON_CLICKED, this.onSettingsButtonClicked, this);
         this.closeButton?.on(Input.EventType.TOUCH_END, this.onCloseButtonClicked, this);
     }
 
     protected onDestroy(): void {
+        this.node.parent.off(EventType.SETTINGS_BUTTON_CLICKED, this.onSettingsButtonClicked, this);
         this.closeButton?.off(Input.EventType.TOUCH_END, this.onCloseButtonClicked, this);
+    }
+
+    onSettingsButtonClicked() {
+        this.node.active = true;
+        director.emit(EventType.SETTINGS_PANEL_OPENED);
     }
 
     onCloseButtonClicked() {
