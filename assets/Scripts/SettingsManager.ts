7fcb9b62e96f8794288082237678464f294a2a4b
1944348723 <133476454+1944348723@users.noreply.github.com>
2025-06-30 11:01:35 +0800

    refactor: refactor settings system

--- assets/Scripts/SettingsManager.ts
+++ assets/Scripts/SettingsManager.ts
@@ -1,33 +1,12 @@
-import { _decorator, Component, director, Input, Node } from 'cc';
-import { EventType } from './EventType';
+import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('SettingsManager')
 export class SettingsManager extends Component {
     @property({type: Node})
-    settingsButton: Node = null;
-
-    @property({type: Node})
     settingsPanel: Node = null;
-
-    protected onLoad(): void {
-        this.settingsButton?.on(Input.EventType.TOUCH_END, this.onSettingsButtonClicked, this);
-    }
-
-    protected onDestroy(): void {
-        this.settingsButton?.off(Input.EventType.TOUCH_END, this.onSettingsButtonClicked, this);
-    }
 
     protected start(): void {
         this.settingsPanel.active =false;
     }
-
-    onSettingsButtonClicked() {
-        if (this.settingsPanel) {
-            this.settingsPanel.active = true;
-        }
-        director.emit(EventType.SETTINGS_PANEL_OPENED);
-    }
 }
-
-
